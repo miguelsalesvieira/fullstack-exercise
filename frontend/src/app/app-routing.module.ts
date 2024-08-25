014d3b2abd8a7811b80035dae13f6914d389819f
Miguel Vieira <miguel.sales.vieira@gmail.com>
2024-08-25 17:53:08 +0200

    feat(pages): added landing page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
