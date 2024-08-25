478b8a2299453ea505b81c200da98f2d2141d02d
Miguel Vieira <miguel.sales.vieira@gmail.com>
2024-08-25 17:56:51 +0200

    feat(pages): added generator page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { GeneratorPageComponent } from './pages/generator-page/generator-page.component';
 
 @NgModule({
-  declarations: [AppComponent, LandingPageComponent],
+  declarations: [AppComponent, LandingPageComponent, GeneratorPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
