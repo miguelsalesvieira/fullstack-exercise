49f391b1b84bdd14f6b32b1d8b33dcb7c52fa56a
Miguel Vieira <miguel.sales.vieira@gmail.com>
2024-08-27 10:07:51 +0200

    feat(types): added backend types

---
+++ backend/src/types.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+export enum ControllerType {
+    GET = "get",
+    POST = "post",
+    PUT = "put",
+    DELETE = "delete",
+}
+
+export type Controller = {
+    path: string;
+    type: ControllerType;
+    func: (req: Request, res: Response) => void;
+};
+
+export type Grid = string[][];
+
+export type Code = {
+    code0: number;
+    code1: number;
+};
