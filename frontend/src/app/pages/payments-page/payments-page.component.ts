6c8a933d3c5c60a96e1921f8adf0cef73f6fe643
Miguel Vieira <miguel.sales.vieira@gmail.com>
2024-08-27 16:21:41 +0200

    feat(pages): added payments page

---
+++ frontend/src/app/pages/payments-page/payments-page.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { PaymentsService } from 'src/app/services/payments.service';
+
+@Component({
+  selector: 'app-payments-page',
+  templateUrl: './payments-page.component.html',
+  styleUrls: ['./payments-page.component.css'],
+})
+export class PaymentsPageComponent {
+  public paymentName: string = '';
+  public paymentAmount: number = 0;
+
+  constructor(public readonly paymentsService: PaymentsService) {}
+
+  handleOnChangeName(name: string) {
+    this.paymentName = name;
+  }
+
+  handleOnChangeAmount(amount: number) {
+    this.paymentAmount = amount;
+  }
+
+  handleOnClickAdd() {
+    if (this.paymentName === '' || this.paymentAmount === 0) {
+      console.error('Invalid input');
+      return;
+    }
+
+    this.paymentsService.addPayment({
+      name: this.paymentName,
+      amount: this.paymentAmount,
+    });
+    this.paymentName = '';
+    this.paymentAmount = 0;
+  }
+}
