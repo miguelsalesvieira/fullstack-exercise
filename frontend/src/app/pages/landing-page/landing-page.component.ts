e1aadb55f6df4737c4dac9c5d5afed8b521292ae
Miguel Vieira <miguel.sales.vieira@gmail.com>
2024-08-27 10:00:36 +0200

    feat(pages): landing page

--- frontend/src/app/pages/landing-page/landing-page.component.ts
+++ frontend/src/app/pages/landing-page/landing-page.component.ts
@@ -5,8 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent {
   constructor() {}
-
-  ngOnInit(): void {}
 }
