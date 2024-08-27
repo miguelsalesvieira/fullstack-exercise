15471881e173efa2f1693ee1b68c847d65919333
Miguel Vieira <miguel.sales.vieira@gmail.com>
2024-08-27 16:25:46 +0200

    refactor(controllers): changed grid to accept bias from query

--- backend/src/controllers/grid.ts
+++ backend/src/controllers/grid.ts
@@ -1,20 +1,21 @@
 import { Request, Response } from "express";
 import { Controller, ControllerType } from "../types";
-import { generateGrid } from "../services/grid";
-import {
-    getCharacter,
-    getCode,
-    separateDigits,
-    validBias,
-} from "../services/code";
+import { generateGridWithCode } from "../services/grid";
+import { validBias } from "../services/code";
 import { environment } from "../services/environment";
-
-let bias = "";
 
 export const gridGet: Controller = {
     path: "/grid",
     type: ControllerType.GET,
     func: (req: Request, res: Response) => {
+        const bias = req.query.bias as string | undefined;
+
+        // Check if bias is valid if it exists
+        if (bias && !validBias(bias)) {
+            res.status(400).send("Invalid bias");
+            return;
+        }
+
         // Get current seconds
         const now = new Date();
         const clock = {
@@ -23,54 +24,18 @@ export const gridGet: Controller = {
             seconds: now.getSeconds(),
         };
 
-        // Generate grid
-        const grid = generateGrid(
+        // Generate grid with code
+        const { grid, code } = generateGridWithCode(
             environment.grid.rows,
             environment.grid.cols,
+            clock.seconds,
             bias
         );
-
-        // Get the grid's characters from the seconds
-        const { digit0, digit1 } = separateDigits(
-            clock.seconds
-        );
-        const character1 = getCharacter(grid, {
-            x: digit0,
-            y: digit1,
-        });
-        const character2 = getCharacter(grid, {
-            x: digit1,
-            y: digit0,
-        });
-
-        // Get the code for the characters
-        const code = getCode(grid, character1, character2);
 
         res.json({
             grid,
             code,
             clock,
         });
-    },
-};
-
-export const gridPost: Controller = {
-    path: "/grid",
-    type: ControllerType.POST,
-    func: (req: Request, res: Response) => {
-        const newBias = req.body.bias;
-        if (!newBias) {
-            res.json({ bias: "" });
-            return;
-        }
-        if (validBias(newBias)) {
-            bias = newBias;
-            res.json({ bias });
-        } else {
-            res.status(400).json({
-                error: "Invalid bias",
-                bias,
-            });
-        }
     },
 };
